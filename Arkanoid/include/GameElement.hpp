82d693d030108e6fac0758ae245090694a1e36b2
R. Kaleta <rafkaleta@onet.pl>
2020-04-28 23:29:00 +0200

    Class fields style

--- Arkanoid/include/GameElement.hpp
+++ Arkanoid/include/GameElement.hpp
@@ -17,20 +17,6 @@ void loadBuffer(GLuint vb, GLuint cb);
 
 class GameBoard
 {
-private:
-    const GLfloat vbDataHexagon[16];
-    const GLfloat cbDataHexagon[24];
-    const GLfloat vbDataTriangle[6];
-    const GLfloat cbDataTriangle[9];
-    GLuint vertexBufferHexagon;
-    GLuint colorBufferHexagon;
-    GLuint vertexBufferTriangle;
-    GLuint colorBufferTriangle;
-
-    mat2 sc;
-    mat2 rt;
-    vec2 tr;
-
 public:
     std::vector<vec2> normVecs;
 
@@ -42,22 +28,23 @@ public:
 
 private:
     void drawOneTriangle(GLuint pID);
+
+    const GLfloat vbDataHexagon[16];
+    const GLfloat cbDataHexagon[24];
+    const GLfloat vbDataTriangle[6];
+    const GLfloat cbDataTriangle[9];
+    GLuint vertexBufferHexagon;
+    GLuint colorBufferHexagon;
+    GLuint vertexBufferTriangle;
+    GLuint colorBufferTriangle;
+
+    mat2 scaleMatrix;
+    mat2 rotateMatrix;
+    vec2 transformVector;
 };
 
 class GameBrick
 {
-private:
-    const GLfloat vbDataRect[8];
-    GLfloat cbDataRect[12];
-    const GLfloat cbDataRectBorder[12];
-    GLuint vertexBufferRect;
-    GLuint colorBufferRect;
-    GLuint colorBufferRectBorder;
-
-    mat2 sc;
-    mat2 rt;
-    vec2 tr;
-
 public:
     std::vector<std::vector<bool>> isVisible;
     int bricksLeft;
@@ -69,21 +56,21 @@ public:
 private:
     void drawRect(GLuint pID, int col);
     void drawRectBorder(GLuint pID);
+
+    const GLfloat vbDataRect[8];
+    GLfloat cbDataRect[12];
+    const GLfloat cbDataRectBorder[12];
+    GLuint vertexBufferRect;
+    GLuint colorBufferRect;
+    GLuint colorBufferRectBorder;
+
+    mat2 scaleMatrix;
+    mat2 rotateMatrix;
+    vec2 transformVector;
 };
 
 class GamePaddle
 {
-private:
-    const GLfloat vbDataPaddle[20];
-    const GLfloat cbDataPaddle[30];
-    GLuint vertexBufferPaddle;
-    GLuint colorBufferPaddle;
-
-    mat2 sc;
-    mat2 rt;
-    vec2 tr;
-    GLfloat velocity;
-
 public:
     GamePaddle();
 
@@ -93,32 +80,21 @@ public:
     void moveRight(GLfloat delta);
     GLfloat getPosX();
     GLfloat getSurf();
+
+private:
+    const GLfloat vbDataPaddle[20];
+    const GLfloat cbDataPaddle[30];
+    GLuint vertexBufferPaddle;
+    GLuint colorBufferPaddle;
+
+    mat2 scaleMatrix;
+    mat2 rotateMatrix;
+    vec2 transformVector;
+    GLfloat velocity;
 };
 
 class GameBall
 {
-private:
-    const GLfloat vbDataBall[28];
-    const GLfloat cbDataBall[42];
-    const GLfloat vbDataCross[8];
-    const GLfloat cbDataCross[12];
-    GLuint vertexBufferBall;
-    GLuint colorBufferBall;
-    GLuint vertexBufferCross;
-    GLuint colorBufferCross;
-
-    mat2 sc;
-    mat2 rt;
-    vec2 tr;
-    vec2 velocity;
-    vec2 vNorm;
-    int angleMult;
-    GLfloat radius;
-    GLfloat separator;
-    GLfloat velDist;
-    bool startingShot;
-    std::vector<std::vector<std::pair<bool, bool>>> collided;
-
 public:
     GameBall();
 
@@ -132,6 +108,28 @@ public:
     void checkCollisionPaddle(GamePaddle & paddle);
     void checkCollisionBrick(GameBrick & brick);
     void moveBall(GLfloat delta);
+
+private:
+    const GLfloat vbDataBall[28];
+    const GLfloat cbDataBall[42];
+    const GLfloat vbDataCross[8];
+    const GLfloat cbDataCross[12];
+    GLuint vertexBufferBall;
+    GLuint colorBufferBall;
+    GLuint vertexBufferCross;
+    GLuint colorBufferCross;
+
+    mat2 scaleMatrix;
+    mat2 rotateMatrix;
+    vec2 transformVector;
+    vec2 velocity;
+    vec2 vNorm;
+    int angleMult;
+    GLfloat radius;
+    GLfloat separator;
+    GLfloat velocityDistance;
+    bool startingShot;
+    std::vector<std::vector<std::pair<bool, bool>>> collided;
 };
 
 #endif
