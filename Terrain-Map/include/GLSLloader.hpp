2a868ffe8e34c0420d30f92d5486fc7457545c8b
R. Kaleta <rafkaleta@onet.pl>
2021-11-07 14:00:26 +0100

    New shaders loading

--- Terrain-Map/include/GLSLloader.hpp
+++ Terrain-Map/include/GLSLloader.hpp
@@ -2,26 +2,18 @@
 #define GLSL_LOADER_HPP_
 
 #include <cstdlib>
-#include <cstring>
 #include <exception>
-#include <fstream>
-#include <iostream>
 #include <stdexcept>
-#include <algorithm>
 #include <string>
 #include <tuple>
-#include <vector>
 #include <GL/glew.h>
-#include <glm/glm.hpp>
 
-using namespace glm;
-
-GLuint compileShader(GLenum shaderType, const std::string & shaderCode,
-                     const std::string & shaderName);
-
-GLuint linkProgram(GLuint vertexShaderID, GLuint fragmentShaderID);
-
-std::string readShader(const std::string & filePath);
+struct shader_error : std::runtime_error
+{
+    explicit shader_error(const std::string & s) : std::runtime_error(s)
+    {
+    }
+};
 
 std::tuple<GLuint, GLuint> loadShaders();
 
